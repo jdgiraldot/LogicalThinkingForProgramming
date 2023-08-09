45e8f1dc161cab2782279c35d6bcda3bf4f7d02d
Jhonatan <jdgiraldot@outlook.com>
2023-08-09 17:39:36 -0500

    Corrección de errores version base

--- js/exercises.js
+++ js/exercises.js
@@ -1,4 +1,5 @@
-// Solucion del ejercicio 1
+// Solución Ejercicio 1
+
 function solucionEjercicio1() {
     let num1 = parseInt(document.getElementById('num1').value)
     let num2 = parseInt(document.getElementById('num2').value)
@@ -6,3 +7,5 @@ function solucionEjercicio1() {
     
     respuesta.innerHTML = "El resultado es: "+ parseInt(`${num1 - num2}${num1 + num2}`)
 }
+
+// Solución Ejercicio 2
